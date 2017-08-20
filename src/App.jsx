c2bfd5665e4adf8026f8869ff23dca52a3d8636f
Mikhail Malyshev <mlshv42@ya.ru>
2017-08-20 19:24:26 +0300

    feat: make App.js data root for notes

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,82 @@
-import React from 'react';
+/* eslint-disable no-underscore-dangle */
+import React, { Component } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Header from './Header';
 import ApiManager from './ApiManager';
 import NoteEditor from './Notes/Editor';
-import NotesList from './Notes/List';
+import Notes from './Notes/List';
 
-const App = () =>
-  (<BrowserRouter>
-    <div>
-      <Header />
-      <Switch>
-        <Route
-          exact
-          path="/"
-          render={() =>
-            (<div>
-              <NoteEditor handleSave={ApiManager.saveNote} />
-              <NotesList fetchNotes={ApiManager.fetchNotes} handleDelete={ApiManager.deleteNote} />
-            </div>)}
-        />
-      </Switch>
-    </div>
-  </BrowserRouter>);
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      notes: [],
+    };
+    this.update = this.update.bind(this);
+    this.handleDelete = this.handleDelete.bind(this);
+    this.handleSave = this.handleSave.bind(this);
+  }
+
+  componentDidMount() {
+    this.update();
+  }
+
+  update() {
+    ApiManager.fetchNotes().then(
+      (res) => {
+        this.setState({ notes: res.data });
+      },
+      () => {
+        console.log("can't update");
+      },
+    );
+  }
+
+  handleSave(note) {
+    return new Promise((resolve, reject) =>
+      ApiManager.saveNote(note).then(() => {
+        this.update();
+        resolve();
+      }, reject),
+    );
+  }
+
+  handleDelete(noteIndex) {
+    const removeNote = (index) => {
+      this.setState(prevState => ({
+        notes: [...prevState.notes.slice(0, index), ...prevState.notes.slice(index + 1)],
+      }));
+    };
+    ApiManager.deleteNote(this.state.notes[noteIndex]._id).then(
+      () => {
+        removeNote(noteIndex);
+      },
+      () => {
+        alert('Ошибочка вышла');
+      },
+    );
+  }
+
+  render() {
+    return (
+      <BrowserRouter>
+        <div>
+          <Header />
+          <Switch>
+            <Route
+              exact
+              path="/"
+              render={() =>
+                (<div>
+                  <NoteEditor handleSave={this.handleSave} />
+                  <Notes notes={this.state.notes} handleDelete={this.handleDelete} />
+                </div>)}
+            />
+          </Switch>
+        </div>
+      </BrowserRouter>
+    );
+  }
+}
 
 export default App;
--- src/Notes/List.jsx
+++ src/Notes/List.jsx
@@ -1,67 +1,32 @@
 /* eslint-disable no-underscore-dangle */
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import Note from './Note';
 
 const ListSt = styled.section`margin: 0 auto;`;
 
-class List extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      notes: [],
-    };
-    this.handleDelete = this.handleDelete.bind(this);
-  }
-
-  componentDidMount() {
-    this.props.fetchNotes().then((response) => {
-      this.setState({ notes: response.data });
-    });
-  }
-
-  handleDelete(noteIndex) {
-    const removeNote = (index) => {
-      this.setState(prevState => ({
-        notes: [...prevState.notes.slice(0, index), ...prevState.notes.slice(index + 1)],
-      }));
-    };
-    this.props.handleDelete(this.state.notes[noteIndex]._id).then(
-      () => {
-        removeNote(noteIndex);
-      },
-      () => {
-        alert('Ошибочка вышла');
-      },
-    );
-  }
-
-  render() {
-    return (
-      <div className="container">
-        <div className="row">
-          <div className="col-xs-12 col-md-8 col-md-offset-2">
-            <ListSt>
-              {this.state.notes.map((note, index) =>
-                (<Note
-                  title={note.title}
-                  text={note.text}
-                  id={index}
-                  key={note._id}
-                  handleDelete={this.handleDelete}
-                />),
-              )}
-            </ListSt>
-          </div>
-        </div>
+const List = props =>
+  (<div className="container">
+    <div className="row">
+      <div className="col-xs-12 col-md-8 col-md-offset-2">
+        <ListSt>
+          {props.notes.map((note, index) =>
+            (<Note
+              title={note.title}
+              text={note.text}
+              id={index}
+              key={note._id}
+              handleDelete={props.handleDelete}
+            />),
+          )}
+        </ListSt>
       </div>
-    );
-  }
-}
+    </div>
+  </div>);
 
 List.propTypes = {
-  fetchNotes: PropTypes.func.isRequired,
+  notes: PropTypes.array.isRequired,
   handleDelete: PropTypes.func.isRequired,
 };
 
