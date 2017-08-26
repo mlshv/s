ffc1bf3189e8acb6363d555e95a876e3f7c81d3d
Mikhail Malyshev <mlshv42@ya.ru>
2017-08-26 21:17:21 +0300

    fix: set correct URL for static assets

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use(express.static(path.resolve(__dirname, '.', 'build')));
+app.use(express.static(path.resolve(__dirname, 'build')));
 
 router.get('/', (req, res) => {
   res.json({ message: 'API Initialized!' });
