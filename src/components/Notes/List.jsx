f1bb049ee15579658d92f322b0824d4ddfa3efda
Mikhail Malyshev <mlshv42@ya.ru>
2017-10-31 18:25:48 +0300

    Minor layout fixes

--- src/components/Notes/List.jsx
+++ src/components/Notes/List.jsx
@@ -1,22 +1,29 @@
 /* eslint-disable no-underscore-dangle */
 import React from 'react';
+import styled from 'styled-components';
 import PropTypes from 'prop-types';
 import Note from './Note';
 
+const ListStyled = styled.div`
+  padding-bottom: .5rem;
+`;
+
 const List = props => (
-  <div className="container-fluid">
-    <div className="row">
-      {props.notes.map((note, index) => (
-        <div className="col-xs-12 col-md-4 col-lg-3" key={note._id}>
-          <Note
-            title={note.title}
-            text={note.text}
-            handleDelete={() => props.handleDelete(note._id, index)}
-          />
-        </div>
-      ))}
+  <ListStyled>
+    <div className="container-fluid">
+      <div className="row">
+        {props.notes.map((note, index) => (
+          <div className="col-xs-12 col-md-4 col-lg-3" key={note._id}>
+            <Note
+              title={note.title}
+              text={note.text}
+              handleDelete={() => props.handleDelete(note._id, index)}
+            />
+          </div>
+        ))}
+      </div>
     </div>
-  </div>
+  </ListStyled>
 );
 
 List.propTypes = {
